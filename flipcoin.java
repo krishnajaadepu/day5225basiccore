48c912db634284f0e0f02d131e0fcf66c4f19bd6
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-11-24 07:34:32 +0530

    Update flipcoin.java

--- flipcoin.java
+++ flipcoin.java
@@ -1,4 +1,10 @@
 package com.bridge.com;
+/*Flip Coin and print percentage of Heads and Tails
+a. I/P -> The number of times to Flip Coin. Ensure it is a positive integer.
+b. Logic -> Use Random Function to get value between 0 and 1. If < 0.5 then tails or
+heads
+c. O/P -> Percentage of Head vs Tails*/
+
 import java.util.Scanner;
 public class flipcoin {
 	static void flipPerc(int times) {
