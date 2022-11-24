e6d647f38d61ee88a2628c84124c69e4d31d159f
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-11-24 07:28:16 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -1,4 +1,8 @@
 package com.bridge.com;
+/* Leap Year
+a. I/P -> Year, ensure it is a 4 digit number.
+b. Logic -> Determine if it is a Leap Year.
+c. O/P -> Print the year is a Leap Year or not.*/
 
 public class Leapyear {
 	
