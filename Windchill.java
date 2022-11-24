5787860d7dcdf87acb689d6488f29a7b560e6d99
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-11-24 07:32:26 +0530

    Update Windchill.java

--- Windchill.java
+++ Windchill.java
@@ -1,4 +1,8 @@
 package com.bridge.com;
+/* Write a program WindChill.java that takes two double command-line arguments t
+and v and prints the wind chill. Use Math.pow(a, b) to compute ab. Given the
+temperature t (in Fahrenheit) and the wind speed v (in miles per hour), the
+National Weather Service defines the effective temperature (the wind chill) to be:*/
 
 public class Windchill {
 	public static void main(String[] args) {
