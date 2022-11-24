8ff93c7c6543aa59ca6acd5ec5db8dc294f9efae
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-11-24 07:41:35 +0530

    Update twoDArrray.java

--- twoDArrray.java
+++ twoDArrray.java
@@ -1,5 +1,11 @@
 package com.bridge.com;
-
+/*2D Array
+a. Desc -> A library for reading in 2D arrays of integers, doubles, or booleans from
+standard input and printing them out to standard output.
+b. I/P -> M rows, N Cols, and M * N inputs for 2D Array. Use Java Scanner Class
+c. Logic -> create 2 dimensional array in memory to read in M rows and N cols
+d. O/P -> Print function to print 2 Dimensional Array. In Java use PrintWriter with
+OutputStreamWriter to print the output to the screen.*/
 import java.util.Scanner;
 
 public class twoDArrray {
