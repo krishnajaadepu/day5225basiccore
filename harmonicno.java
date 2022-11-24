8b8512b743142d663329353a0490aacf62cfece3
krishnajaadepu <113959348+krishnajaadepu@users.noreply.github.com>
2022-11-24 07:35:38 +0530

    Update harmonicno.java

--- harmonicno.java
+++ harmonicno.java
@@ -1,4 +1,10 @@
 package com.bridge.com;
+/*Harmonic Number
+a. Desc -> Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+(http://users.encs.concordia.ca/~chvatal/notes/harmonic.html).
+b. I/P -> The Harmonic Value N. Ensure N != 0
+c. Logic -> compute 1/1 + 1/2 + 1/3 + ... + 1/N
+d. O/P -> Print the Nth Harmonic Value.*/
 
 import java.util.Scanner;
 public class harmonicno {
